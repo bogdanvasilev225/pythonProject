50c440960f7a45e45a4915670895cbf84d21befb
Bogdan Vasilev <bogdanvasilev225@gmail.com>
2024-01-08 14:26:47 +0300

    добавлено 14 домашнее задание

--- homework14.py
+++ homework14.py
@@ -3,7 +3,27 @@
 Выведите первый символ строки sentence.
 Выведите последний символ строки sentence.
 Создайте еще одну строку new_sentence и объедините её с sentence.
-Используя форматированную строку, создайте строку greeting, которая приветствует пользователя по имени и возрасту.
+Используя форматированную строку, создайте строку greeting,
+ которая приветствует пользователя по имени и возрасту.
 Используя метод replace(), замените все буквы 'a' в строке sentence на 'x'.
-Используя срез, получите подстроку из sentence, содержащую слово "World".
 """
+sentence = "Bogdan"
+print("Вывод строки:", sentence)
+print("Вывод первого символа:", sentence[0])
+print("Вывод последнего символа:", sentence[-1])
+
+new_sentence = "Vasilev"
+combined_sentence = sentence + " " + new_sentence
+print("Вывод объедененных строк", combined_sentence)
+
+modified_sentence = sentence.replace('a', 'x')
+print("Строка после замены:", modified_sentence)
+
+
+name = input("Введите имя:")
+age = int(input("Введите возраст:"))
+greeting = f"Привет, меня зовут {name} мне {age} лет"
+print(greeting)
+
+
+
